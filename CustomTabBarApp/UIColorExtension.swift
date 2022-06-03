f1106ae498c51607d435371b8af943a4d07a189f
Anna <anna.vasi4ko@icloud.com>
2022-06-03 16:26:36 +0300

    custom tab bar

---
+++ CustomTabBarApp/UIColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExtension.swift
+//  CustomTabBarApp
+//
+//  Created by Vasichko Anna on 03.06.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static func tabBarItemAccent() -> UIColor {
+        #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+    }
+    static func mainWhite() -> UIColor {
+        #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+    }
+    static func tabBarItemLight() -> UIColor {
+        #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 0.4914631623)
+    }
+}
